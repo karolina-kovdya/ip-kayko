6a7a8fbe85ecc5829eca049535aaf6f66285d00f
Каролина Ковдя-Оникиенко <karolinakovda-onikienko@Karolinas-MacBook-Pro.local>
2025-10-13 19:20:20 +0300

    change btn in Aboute

--- src/components/Aboute/Aboute.js
+++ src/components/Aboute/Aboute.js
@@ -7,14 +7,14 @@ function Aboute() {
                 <h1 className='aboute__title'>Включим вашу продукцию<br></br> в реестры Минпромторга</h1>
                 <h2 className='aboute__subtitle'>Сопровождаем полный цикл процедуры по 719, 616, 878 ПП<br></br> для получения Заключения Минпромторга на вашу продукцию</h2>
                 <div className='aboute__btns'>
-                    <div className='aboute__block'>
-                        <button className='aboute__btn'></button>
-                        <p className='aboute__btn-text'>Оставить заявку</p>
-                    </div>
-                    <div className='aboute__block'>
-                        <button className='aboute__btn'></button>
-                        <p className='aboute__btn-text'>Подробнее</p>
-                    </div>
+                    <button className='aboute__btn'>
+                        <span className='aboute__circle'></span>
+                        <span className='aboute__btn-text'>Оставить заявку</span>
+                    </button>
+                    <button className='aboute__btn'>
+                        <span className='aboute__circle'></span>
+                        <span className='aboute__btn-text'>Подробнее</span>
+                    </button>
                 </div>
             </div>
         </section>

1a9aae87a5f7b6a874498cfed754caeeca095bda
Каролина Ковдя-Оникиенко <karolinakovda-onikienko@Karolinas-MacBook-Pro.local>
2025-10-13 19:45:30 +0300

    add nav to WhatsApp from Aboute

--- src/components/Aboute/Aboute.js
+++ src/components/Aboute/Aboute.js
@@ -7,10 +7,17 @@ function Aboute() {
                 <h1 className='aboute__title'>Включим вашу продукцию<br></br> в реестры Минпромторга</h1>
                 <h2 className='aboute__subtitle'>Сопровождаем полный цикл процедуры по 719, 616, 878 ПП<br></br> для получения Заключения Минпромторга на вашу продукцию</h2>
                 <div className='aboute__btns'>
-                    <button className='aboute__btn'>
+                    <a
+                      className='aboute__link' 
+                      href='https://wa.me/+79190532886'
+                      target='blanc'
+                      title='Перейти в WhatsApp'
+                    >
+                      <button className='aboute__btn'>
                         <span className='aboute__circle'></span>
                         <span className='aboute__btn-text'>Оставить заявку</span>
-                    </button>
+                      </button>
+                    </a>
                     <button className='aboute__btn'>
                         <span className='aboute__circle'></span>
                         <span className='aboute__btn-text'>Подробнее</span>
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
@@ -8,6 +9,9 @@ function App() {
     <div className="App">
       <Header />
       <Main />
+      <Routes>
+        
+      </Routes>
       <Footer />
     </div>
   );

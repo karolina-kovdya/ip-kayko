adfc4693d3ab02ab4d7a1a0ccb50cc96a5bd500f
Каролина Ковдя-Оникиенко <karolinakovda-onikienko@Karolinas-MacBook-Pro.local>
2025-10-14 21:31:21 +0300

    add mobile view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,28 +8,30 @@ function Header() {
       <div className='header__container'>
         <div className='header__info'>
           <img
-          className='header__logo'
-          alt='логотип'
-          src={logo}
-        />
+            className='header__logo'
+            alt='логотип'
+            src={logo}
+          />
           <p className='header__text'>ИП Кайко О.В</p>
         </div>
         <div className='header__block'>
           <div className='header__info'>
             <div className='header__info-web'>
-              <a
-                className='header__link'
-                href='https://t.me/+79190532886'
-                title='Написать в Telegram'
-                target='blanc'
-              >
-                <img
-                  className='header__icon'
-                  alt='иконка телеграм'
-                  src={icon}
-                />
-              </a>
-              <a className='header__num' href="tel:+79190532886"  title='Позвонить'>+79190532886</a>
+              <div className='header__info-box'>
+                <a
+                  className='header__link'
+                  href='https://t.me/+79190532886'
+                  title='Написать в Telegram'
+                  target='blanc'
+                >
+                  <img
+                    className='header__icon'
+                    alt='иконка телеграм'
+                    src={icon}
+                  />
+                </a>
+                <a className='header__num' href="tel:+79190532886" title='Позвонить'>+79190532886</a>
+              </div>
               <a
                 className='header__link'
                 href='https://wa.me/+79190532886'
